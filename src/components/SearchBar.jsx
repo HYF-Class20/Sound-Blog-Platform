86af43338f57bfe06810d44a33f0eae60c470473
mervat abuamro <mervat.abuamro@gmail.com>
2023-11-26 14:41:54 +0100

    adding search bar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,32 +7,50 @@ const SearchBar = () => {
   const [soundList, setSoundList] = useState([])  
   const [input, setInput] = useState ('');
 
+  const fetchRecords = async () => {
+    try {
+      const response = await axios.get('http://localhost:5010/audio')
+      const data = response.data.result;
+      setSoundList(data);
+    } catch (error) {
+      console.error( 'Error fetching records:', error);
+      return;
+    }
+  };
+  
+
+
    useEffect (() => {
-    axios.get('')
-    .then((response) => setSoundList(response.data.results)), []
-   })
+    fetchRecords()
+
+   }, [])
+
   return (
     <div className='input'>
-
-        < input className= 'search-bar' type= 'text' placeholder='search...'
-  
-        onChange={(e) => setInput(e.target.value)}
-        />
-        {soundList.filter((item) => {
-          if (input === ''){
-            return item
-          } else if (item.title.toLowerCase().includes(input.toLowerCase())){
-           return item
-          }
-        })
-        .map((item) => {
-          return <div className='audio-container' key={item.id}>
-            <h2>{item.title}</h2>
-            <h2>{item.genre}</h2>
-            <h3>{item.author}</h3>
+    <input
+      className='search-bar'
+      type='text'
+      placeholder='search...'
+      onChange={(e) => setInput(e.target.value)}
+    />
+    {input.length > 0 && (
+      <div>
+        {soundList
+          .filter((item) =>
+            item.title.toLowerCase().includes(input.toLowerCase())
+          )
+          .map((item) => (
+            <div className='audio-container' key={item.id}>
+              <h2>{item.title}</h2>
+              <h2>{item.genre}</h2>
+              <h3>{item.author}</h3>
             </div>
-        })}
-        </div>
+          ))}
+      </div>
+    )}
+  </div>
+  
+
   )
 }
 
