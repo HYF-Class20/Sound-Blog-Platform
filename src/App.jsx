d91acfd32a22f89e025d79cdcfd36bb7f4b53414
Rex Lou <rexlou@Rexs-MacBook-Air.local>
2023-11-26 17:36:39 +0100

    trying to fix conflict #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import Category from '../Component/Category/Category';
-import axios from "axios";
-import { useState, useEffect } from "react";
+
+
 
 const App = ()=>{
-     const [records, setRecords] = useState([]);
-     const [tracks, setTracks] = useState([]);
-     const [track, setTrack] = useState(null);
+    
    
-     const fetchRecords = async () => {
-       try {
-         const response = await axios.get("http://localhost:5010/audio");
-         const data = response.data.result;
-   
-         setRecords(data);
-         setTracks([...data]);
-       } catch (error) {
-         console.error("Error fetching records:", error);
-         return;
-       }
-     };
-   
-     useEffect(() => {
-       fetchRecords();
-     }, []);
      return (
       <>
-      <Category records={records} setTracks={setTracks} />
+      <Category />
       
       </>
      )
