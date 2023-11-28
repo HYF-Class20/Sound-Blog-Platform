342f2618362112ac957834762e4cbd2c4b24c8ee
mervat abuamro <mervat.abuamro@gmail.com>
2023-11-28 20:27:58 +0100

    updating search bar component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,58 +1,26 @@
-import React, { useCallback } from 'react'
-import { useEffect, useState } from 'react'
-import axios from 'axios';
-import './SearchBar.css'
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const SearchBar = () => {
-  const [soundList, setSoundList] = useState([])  
-  const [input, setInput] = useState ('');
+export default function SearchBar({ setTitle }) {
+  const [input, setInput] = useState(null);
 
-  const fetchRecords = async () => {
-    try {
-      const response = await axios.get('http://localhost:5010/audio')
-      const data = response.data.result;
-      setSoundList(data);
-    } catch (error) {
-      console.error( 'Error fetching records:', error);
-      return;
-    }
-  };
-  
-
-
-   useEffect (() => {
-    fetchRecords()
-
-   }, [])
+  useEffect(() => {
+    setTitle(input);
+  }, [input]);
 
   return (
-    <div className='input'>
-    <input
-      className='search-bar'
-      type='text'
-      placeholder='search...'
-      onChange={(e) => {setInput(e.target.value)}}
-    />
-    {input.length > 0 && (
-      <div>
-        {soundList
-          .filter((item) =>
-            item.title.toLowerCase().includes(input.toLowerCase())
-
-          )
-          .map((item) => (
-            <div className='audio-container' key={item.id}>
-              <h2>{item.title}</h2>
-              <h2>{item.genre}</h2>
-              <img src={item.thumbnail} alt={`${item.title} thumbnail`} />
-            </div>
-          ))}
-      </div>
-    )}
-  </div>
-  
-
-  )
+    <div className="input">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="search..."
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+    </div>
+  );
 }
-
-export default SearchBar
+SearchBar.propTypes = {
+  setTitle: PropTypes.func.isRequired,
+};
