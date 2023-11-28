2394911762ce9717b1200cada3dabb8ef4279ebd
mervat abuamro <mervat.abuamro@gmail.com>
2023-11-28 20:35:04 +0100

    updating search bar

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,27 @@
+
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export default function SearchBar({ setTitle }) {
+  const [input, setInput] = useState(null);
+
+  useEffect(() => {
+    setTitle(input);
+  }, [input]);
+
+  return (
+    <div className="input">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="search..."
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+SearchBar.propTypes = {
+  setTitle: PropTypes.func.isRequired,
+};
